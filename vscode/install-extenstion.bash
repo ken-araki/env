ef31380e102a800a377494e385ed553b0714e49b
ken-araki <k.takagaki.614@gmail.com>
2020-04-12 17:10:01 +0900

    add vscode setting

---
+++ vscode/install-extenstion.bash
@@ -0,0 +1,40 @@
+## see
+## [インストールするだけでVSCodeをカッコよくする拡張4つ - Qiita](https://qiita.com/ksakiyama134/items/8ca98295897dc1280644)
+
+## Bracket Pair Colorizer
+## see. https://marketplace.visualstudio.com/items?itemName=CoenraadS.bracket-pair-colorizer
+code --install-extension CoenraadS.bracket-pair-colorizer
+
+## Dracula Theme
+## see. https://draculatheme.com
+code --install-extension gerane.Theme-Dracula
+
+## Indent Rainbow
+## see. https://marketplace.visualstudio.com/items?itemName=oderwat.indent-rainbow
+code --install-extension oderwat.indent-rainbow
+
+## Material Icon Theme
+## see. https://marketplace.visualstudio.com/items?itemName=PKief.material-icon-theme
+code --install-extension PKief.material-icon-theme
+
+## see
+## [VSCodeのオススメ拡張機能 24 選 (とTipsをいくつか)](https://qiita.com/sensuikan1973/items/74cf5383c02dbcd82234)
+
+## GitLens
+## see. https://marketplace.visualstudio.com/items?itemName=eamodio.gitlens
+
+
+## REST CLIENT
+## see. https://marketplace.visualstudio.com/items?itemName=humao.rest-client
+code --install-extension humao.rest-client
+
+## Partial Diff
+## see. https://marketplace.visualstudio.com/items?itemName=ryu1kn.partial-diff
+code --install-extension ryu1kn.partial-diff
+
+## React snippets
+code --install-extension dsznajder.es7-react-js-snippets
+
+## Marp for VSCode
+## see. https://marketplace.visualstudio.com/items?itemName=marp-team.marp-vscode
+code --install-extension marp-team.marp-vscode
